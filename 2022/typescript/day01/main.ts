d506f41907c584132b3dc62be238e634dde4d421
Webber <webber@takken.io>
2022-12-01 14:21:57 +0100

    feat: day1 ts

--- 2022/typescript/day01/main.ts
+++ 2022/typescript/day01/main.ts
@@ -1,3 +1,12 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { input } from "./input.ts";
+
+// Part 1
+const groups = input.split("\n\n");
+const totals = groups.map((group) =>
+  group.split("\n").reduce((acc, number) => acc + Number(number), 0)
+);
+console.log(Math.max(...totals));
+
+// Part 2
+const topThreeTotals = totals.sort((a, b) => b - a).slice(0, 3);
+console.log(topThreeTotals.reduce((acc, number) => acc + number, 0));
