2e2f20f97a68e4b47822cb21d2569d037b1de40f
Webber Takken <webber.nl@gmail.com>
2022-12-04 17:23:24 +0100

    feat: day4 - rust (#6)
    * chore: simplify
    
    * chore: remove flat
    
    * feat: day4 - rust

--- 2022/typescript/day04.ts
+++ 2022/typescript/day04.ts
@@ -3,32 +3,20 @@ import Library from "./lib.ts";
 const getGroups = (): number[][] =>
   Library.getInput("day04")
     .split("\n")
-    .map((pair) =>
-      pair
-        .split(",")
-        .map((range) => range.split("-"))
-        .flat()
-        .map((number) => Number(number))
-    );
+    .map((a) => a.split(/[,-]/).map((number) => Number(number)));
 
 // Part 1
 export const getNumberOfCompleteOverlaps = (): number => {
   return getGroups().reduce((acc, [a, b, c, d]) => {
     const contains = a >= c && b <= d;
-    const completeOverlap = c >= a && d <= b;
-    return contains || completeOverlap ? acc + 1 : acc;
+    const overlaps = a <= c && b >= d;
+    return contains || overlaps ? acc + 1 : acc;
   }, 0);
 };
 console.log("Number of complete overlaps:", getNumberOfCompleteOverlaps());
 
 // Part 2
 export const getNumberOfPartialOverlaps = (): number => {
-  return getGroups().reduce((acc, [a, b, c, d]) => {
-    const leftOverlap = a >= c && a <= d;
-    const rightOverlap = b >= c && b <= d;
-    const completeOverlap = a <= c && b >= d;
-    const contains = a >= c && b <= d;
-    return leftOverlap || rightOverlap || completeOverlap || contains ? acc + 1 : acc;
-  }, 0);
+  return getGroups().reduce((acc, [a, b, c, d]) => (a > d || b < c ? acc : acc + 1), 0);
 };
 console.log("Number of partial overlaps:", getNumberOfPartialOverlaps());
