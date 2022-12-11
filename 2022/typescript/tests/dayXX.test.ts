9395a5f7d2e1c436c5eec1095b751a9465027998
Webber <webber@takken.io>
2022-12-11 01:51:26 +0100

    chore: update templates

---
+++ 2022/typescript/tests/dayXX.test.ts
@@ -0,0 +1,30 @@
+import Library from "../lib.ts";
+import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
+import sinon from "https://cdn.skypack.dev/sinon@11.1.2?dts";
+import { part1, part2 } from "../day...";
+
+Deno.test("Day XX", async (context) => {
+  await context.step("part1", async (test) => {
+    await test.step(`works with example input`, async () => {
+      const getInput = sinon.stub(Library, "getInput").returns(Library.getTestInput("dayXX-example"));
+      assertEquals(part1(), 0);
+      getInput.restore();
+    });
+
+    // await test.step(`works with real input`, async () => {
+    //   assertEquals(part1(), 0);
+    // });
+  });
+
+  await context.step("part2", async (test) => {
+    //   await test.step(`works with example input`, async () => {
+    //     const getInput = sinon.stub(Library, "getInput").returns(Library.getTestInput("dayXX-example"));
+    //     assertEquals(part2(), 0);
+    //     getInput.restore();
+    //   });
+    //
+    //   await test.step(`works with real input`, async () => {
+    //     assertEquals(part2(), 0);
+    //   });
+  });
+});
