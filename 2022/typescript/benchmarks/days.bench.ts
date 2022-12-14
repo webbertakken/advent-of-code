174391886e9ff12364c3e564a6a93478d3e79516
Webber <webber@takken.io>
2022-12-14 22:07:20 +0100

    feat: partial part 1

--- 2022/typescript/benchmarks/days.bench.ts
+++ 2022/typescript/benchmarks/days.bench.ts
@@ -10,6 +10,7 @@ import { getHighestViewScore, getVisibleTrees } from "../day08.ts";
 import { getNinthTailVisitCount, getTailVisitedCount } from "../day09.ts";
 import { decodedLettersAsImage, sumOfSixSignalStrengths } from "../day10.ts";
 import { monkeyBusinessLevelAfter20, monkeyBusinessLevelAfter10k } from "../day11.ts";
+import { fewestStepsRequired } from "../day12.ts";
 
 Lib.bench({ name: "day01 - part 1", fn: highestTotal });
 Lib.bench({ name: "day01 - part 2", fn: sumOfTopThree });
@@ -43,3 +44,5 @@ Lib.bench({ name: "day10 - part 2", fn: decodedLettersAsImage });
 
 Lib.bench({ name: "day11 - part 1", fn: monkeyBusinessLevelAfter20 });
 Lib.bench({ name: "day11 - part 2", fn: monkeyBusinessLevelAfter10k });
+
+Lib.bench({ name: "day12 - part 1", fn: fewestStepsRequired });
