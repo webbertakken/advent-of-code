eb27ac9eadf2b7e62d347129667cb36e5fdd407c
Webber <webber@takken.io>
2024-12-01 17:43:59 +0100

    chore: 2024 day01, improve performance and readability

--- 2024/typescript/day01.ts
+++ 2024/typescript/day01.ts
@@ -3,41 +3,43 @@ import Library from './lib'
 const getInput = () => Library.getInput('day01').split('\n')
 
 const getLists = () => {
-  const listA = []
-  const listB = []
+  const lists: { [key: string]: number[] } = { left: [], right: [] }
 
   for (const [a, b] of getInput().map((line) => line.split(/\s+/).map(Number))) {
-    listA.push(a)
-    listB.push(b)
+    lists.left.push(a)
+    lists.right.push(b)
   }
 
-  listA.sort()
-  listB.sort()
-
-  return [listA, listB]
+  return [lists.left.sort(), lists.right.sort()]
 }
 
 // Part 1
 export const getTotalDistanceBetweenPairs = () => {
-  const [listA, listB] = getLists()
+  const [left, right] = getLists()
 
   let distance = 0
-  for (let i = 0; i < listA.length; ++i) {
-    distance += Math.abs(listA[i] - listB[i])
+  for (let i = 0; i < left.length; ++i) {
+    distance += Math.abs(left[i] - right[i])
   }
 
   return distance
 }
-
 console.log('Total distance between pairs:', getTotalDistanceBetweenPairs())
 
 // Part 2
 export const getTotalSimilarityScore = () => {
-  const [listA, listB] = getLists()
+  const [left, right] = getLists()
 
+  // Count the number of times each number appears in list B
+  const listBNumberCounts = new Map<number, number>()
+  for (const item of right) {
+    listBNumberCounts.set(item, (listBNumberCounts.get(item) ?? 0) + 1)
+  }
+
+  // Get the count of each number in list A and multiply it by the number
   let similarityScore = 0
-  for (const item of listA) {
-    similarityScore += listB.filter((x) => x === item).length * item
+  for (const item of left) {
+    similarityScore += (listBNumberCounts.get(item) ?? 0) * item
   }
 
   return similarityScore
