d8cfda34016ccbdc751f8f7377ece3e66d728c0c
Webber Takken <webber.nl@gmail.com>
2024-12-03 15:47:33 +0100

    feat: 2024 day 3 (#31)

--- 2024/typescript/benchmarks/days.bench.ts
+++ 2024/typescript/benchmarks/days.bench.ts
@@ -1,8 +1,9 @@
-import { part1, part2 } from '../dayXX'
-
 import { benchmark } from '../tests/testLib'
+
+import { part1, part2 } from '../dayXX'
 import { getTotalDistanceBetweenPairs, getTotalSimilarityScore } from '../day01'
 import { analiseSafetyAfterProblemDampener, analyseNumberOfSafeStrangeReports } from '../day02'
+import { getSumOfAllInstructions, getSumOfEnabledInstructions } from '../day03'
 
 benchmark('dayXX - part 1', part1)
 benchmark('dayXX - part 2', part2)
@@ -12,3 +13,6 @@ benchmark('day01 - part 2', getTotalSimilarityScore)
 
 benchmark('day02 - part 1', analyseNumberOfSafeStrangeReports)
 benchmark('day02 - part 2', analiseSafetyAfterProblemDampener)
+
+benchmark('day03 - part 1', getSumOfAllInstructions)
+benchmark('day03 - part 2', getSumOfEnabledInstructions)
