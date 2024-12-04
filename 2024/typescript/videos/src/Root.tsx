f0b25f05058214c7848ec540b82a352a63ec3442
Webber <webber@takken.io>
2024-12-04 16:10:40 +0100

    feat: 2024 day 4 visualisation

--- 2024/typescript/videos/src/Root.tsx
+++ 2024/typescript/videos/src/Root.tsx
@@ -1,9 +1,11 @@
 import { Composition } from 'remotion'
 import { Day03 } from './Day03/Day03'
+import { Day04 } from './Day04/Day04'
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      <Composition id="Day04" component={Day04} width={1920} height={1080} fps={60} durationInFrames={6000} />
       <Composition id="Day03" component={Day03} width={1920} height={1080} fps={60} durationInFrames={6000} />
     </>
   )
