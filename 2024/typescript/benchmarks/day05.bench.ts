dea480308fd6f7195ab4b1a50eaa390c2acca76d
Webber <webber@takken.io>
2024-12-05 16:35:59 +0100

    chore: remove day-specific benchmark

--- 2024/typescript/benchmarks/day05.bench.ts
+++
@@ -1,5 +0,0 @@
-import { benchmark } from '../tests/testLib'
-import { getSumOfMiddlePageNumbersFromCorrectUpdates, getSumOfMiddlePageNumbersFromIncorrectUpdates } from '../day05'
-
-benchmark('day05 - part 1', getSumOfMiddlePageNumbersFromCorrectUpdates)
-benchmark('day05 - part 2', getSumOfMiddlePageNumbersFromIncorrectUpdates)
