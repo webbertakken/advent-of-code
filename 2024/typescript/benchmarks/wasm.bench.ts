207cccba36a253feadddd992c83f760394f0d12e
Webber <webber@takken.io>
2024-12-08 15:16:30 +0100

    feat: 2024 day 8

--- 2024/typescript/benchmarks/wasm.bench.ts
+++ 2024/typescript/benchmarks/wasm.bench.ts
@@ -1,5 +1,9 @@
 import { benchmark } from '../tests/testLib'
 import { typescriptNativeCode, offloadToWasm } from '../dayXX-wasm'
+import { wasmGetRevisedTotalCalibrationResult, wasmGetTotalCalibrationResult } from '../day07-wasm'
 
 benchmark('dayXX - wasm - part 1', typescriptNativeCode)
 benchmark('dayXX - wasm - part 2', offloadToWasm)
+
+benchmark('day07 (wasm) - part 1', wasmGetTotalCalibrationResult)
+benchmark('day07 (wasm) - part 2', wasmGetRevisedTotalCalibrationResult)
