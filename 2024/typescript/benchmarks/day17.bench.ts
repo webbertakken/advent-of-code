6136b2229e97f40910f7e81940ae106bae357884
Webber Takken <webber.nl@gmail.com>
2024-12-18 02:12:52 +0100

    feat: 2024 day 17 (#55)

---
+++ 2024/typescript/benchmarks/day17.bench.ts
@@ -0,0 +1,5 @@
+import { benchmark } from '../tests/testLib'
+import { getProgramStringOutput, findInstructionFixedPoint } from '../day17'
+
+benchmark('day17 - part 1', getProgramStringOutput)
+benchmark('day17 - part 2', findInstructionFixedPoint)
